1ed66863f3930de33c1ec3267f755b7889c002d5
George Papagapitos <george.g.papagapitos@gmail.com>
2021-04-01 14:46:49 -0500

    delete working but buggy

--- pet-hotel/src/components/HistoryTable/HistoryTableItem.jsx
+++ pet-hotel/src/components/HistoryTable/HistoryTableItem.jsx
@@ -1,3 +1,5 @@
+import axios from "axios"
+
 function HistoryTableItem({ item }) {
   // breed: "Hamster"​
   // checked_date: null
@@ -8,6 +10,21 @@ function HistoryTableItem({ item }) {
   // owner_name: "John B"
   // pet_name: "Snuffy"
 
+  const handleDelete = () => {
+    console.log('delete pet with id', item.id)
+    axios.delete(`/api/pets/delete/${item.id}`)
+    .then(res => {
+      console.log('deleted pet');
+    })
+    .catch(err => {
+      console.log('err in delete pet', err)
+    })
+  }
+
+  const handleCheckIn = () => {
+    console.log('check in pet with id', item.id)
+  }
+
   return (
     <tr>
       <td>{item.owner_name}</td>
@@ -15,7 +32,7 @@ function HistoryTableItem({ item }) {
       <td>{item.breed}</td>
       <td>{item.color}</td>
       <td>{item.checked_date ? item.checked_date : 'None'}</td>
-      <td>Delete | Check In</td>
+      <td><button onClick={handleDelete}>Delete</button><button onClick={handleCheckIn}>Check In</button></td>
     </tr>
   );
 }
