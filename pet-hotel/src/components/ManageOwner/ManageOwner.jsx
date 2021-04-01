bd6907c1793fc7e689e819bae44205f7979ecb10
Johnshandsable <Jshands@asu.edu>
2021-04-01 14:30:35 -0500

    set up sagas and redux / tested

--- pet-hotel/src/components/ManageOwner/ManageOwner.jsx
+++ pet-hotel/src/components/ManageOwner/ManageOwner.jsx
@@ -1,7 +1,19 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 import AddOwnerForm from '../AddOwnerForm/AddOwnerForm';
 import OwnerTable from '../OwnerTable/OwnerTableItem';
 
 function ManageOwner() {
+  const dispatch = useDispatch();
+  const owners = useSelector((store) => store.ownerReducer);
+
+  useEffect(() => {
+    dispatch({
+      type: 'GET_OWNERS',
+    });
+  }, []);
+
   return (
     <div>
       <h3>Manage Owners here...</h3>
